88168838b97ea240e05ec959d19c9b3cc0d654b2
Ange230700 <kouakouangeericstephane@gmail.com>
2023-12-18 10:21:24 +0100

    finishing step1

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,15 +1,20 @@
-CREATE TABLE boat (
-  id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
+DROP TABLE IF EXISTS `boat`;
+
+CREATE TABLE `boat` (
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `coord_x` INT NOT NULL,
+  `coord_y` INT NOT NULL,
+  PRIMARY KEY(`id`)
 );
 
-CREATE TABLE tile (
-  id INT AUTO_INCREMENT NOT NULL,
-  type VARCHAR(255) NOT NULL,
-  coord_x INT NOT NULL,
-  coord_y INT NOT NULL,
-  PRIMARY KEY(id)
+DROP TABLE IF EXISTS `tile`;
+
+CREATE TABLE `tile` (
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `type` VARCHAR(255) NOT NULL,
+  `coord_x` INT NOT NULL,
+  `coord_y` INT NOT NULL,
+  `has_treasure` TINYINT(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY(`id`)
 );
